5f0579b19add79e483e5a602dd1dfb826c1edcd0
Admex666 <adam.jakus99@gmail.com>
2025-03-19 20:54:00 +0100

    new variables to ML_PL

--- ML_PL_new/ML_PL_transform_data.py
+++ ML_PL_new/ML_PL_transform_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def df_to_model_input(df):  
     # Writing it out
@@ -14,16 +15,28 @@ def df_to_model_input(df):
     df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
     df.sort_values('Date', inplace=True)
     
+    # Calculate points
+    df['HomePoints'] = np.where(df.FTR == 'H', 3, np.where(df.FTR=='D', 1, 0))
+    df['AwayPoints'] = np.where(df.FTR == 'A', 3, np.where(df.FTR=='D', 1, 0))
+    
+    # Calculate goal differences
+    df['HomeGD'] = df.HomeGoals - df.AwayGoals
+    df['AwayGD'] = df.AwayGoals - df.HomeGoals
+    
     # Stats list for rolling averages
-    stats = ['Goals', 'Shots', 'ShotsOnTarget', 'Corners', 'Yellows']
+    stats = ['Goals', 'Shots', 'ShotsOnTarget', 'Corners', 'Yellows', 'Points', 'GD']
     home_stats = ['Home' + stat for stat in stats]
     away_stats = ['Away' + stat for stat in stats]
     
     # Rolling average calculations for each team
     rolling_features = []
+    ppm_season_dict = {}
     for team in pd.concat([df['HomeTeam'], df['AwayTeam']]).unique():
         team_home = df[df['HomeTeam'] == team].copy()
         team_away = df[df['AwayTeam'] == team].copy()
+        
+        ppm_season_dict[team] = {'Home':team_home.HomePoints.mean(),
+                                 'Away':team_away.AwayPoints.mean()}
     
         for stat, home_stat, away_stat in zip(stats, home_stats, away_stats):
             team_home[stat + '_Home_RAvg'] = team_home[home_stat].shift(1).rolling(window=3).mean()
@@ -48,4 +61,12 @@ def df_to_model_input(df):
     
     # Final data structure
     model_input = final_df.copy().dropna().reset_index(drop=True)
+    
+    # Adding new variables
+    model_input[['HomePPM', 'AwayPPM']] = None, None 
+    for i, row in model_input.iterrows():
+        home_ppm = ppm_season_dict.get(row['HomeTeam']).get('Home')
+        away_ppm = ppm_season_dict.get(row['AwayTeam']).get('Away')
+        model_input.loc[i, ['HomePPM', 'AwayPPM']] = home_ppm, away_ppm
+    
     return model_input
