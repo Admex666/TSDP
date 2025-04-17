db62e4e2750a5b302c177e70e023f0bb75b3ff18
admex666 <adam.jakus99@gmail.com>
2025-04-17 09:57:49 +0200

    b4a visual upgrade

--- fbref/fbref_b4after.py
+++ fbref/fbref_b4after.py
@@ -3,22 +3,23 @@ import fbref.fbref_module as fbref
 
 URL = "https://fbref.com/en/squads/8602292d/2024-2025/matchlogs/all_comps/shooting/Aston-Villa-Match-Logs-All-Competitions"
 table_id = 'matchlogs_for'
+team = URL.split('/')[-1].split('-Match-')[0].replace('-', ' ')
+league = URL.split('Match-Logs-')[-1].replace('-', ' ')
 
 df = fbref.scrape(URL, table_id)
 df = df[(df.Expected_xG != 'Expected') & (df.Expected_xG != 'xG')]
 df.dropna(inplace=True)
 
-# Calculate moving averages
-df['xG_mavg'] = df['Expected_xG'].rolling(window=window).mean()
-df['xGA_ma'] = df['xGA'].rolling(window=window).mean()
 df.rename(columns={[col for col in df.columns if '_Date' in col][0]:'Date'}, inplace=True)
 df['nr'] = range(len(df))
 df['nr'] += 1
 
-df.rename(columns={'Standard_Gls': 'Goals', 'Expected_xG': 'xG'}, inplace=True)
+df.rename(columns={'Standard_Gls': 'Goals', 'Expected_xG': 'xG',
+                   'Standard_G/Sh': 'Goal/Shot', 
+                   'Standard_G/SoT': 'Goal/Shot on Goal'}, inplace=True)
 
 #%% Line chart
-def create_mavg_chart(df, x, col1, col2, title, y_title, b4a_line, b4a_text, save_path, save_name, colors=['blue', 'skyblue'], b4a_color='orange',  window=4, save=False):
+def create_mavg_chart(df, x, col1, col2, y_title, b4a_line, b4a_text, save_path, save_name, url_logo, colors=['blue', 'skyblue'], b4a_color='orange',  window=4, save=False):
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
@@ -37,7 +38,7 @@ def create_mavg_chart(df, x, col1, col2, title, y_title, b4a_line, b4a_text, sav
     my_font_props = font_manager.FontProperties(fname=my_font_path)
     c1, c2 = colors
     
-    plt.figure(figsize=(10, 6), facecolor=background_color)
+    fig = plt.figure(figsize=(10, 6), facecolor=background_color)
     ax = plt.gca()
     ax.set_facecolor(background_color)
     # Draw lines
@@ -90,8 +91,18 @@ def create_mavg_chart(df, x, col1, col2, title, y_title, b4a_line, b4a_text, sav
     
     plt.xlabel('Match number', color='white')
     plt.ylabel(y_title, color='white')
-    plt.title(title, fontsize=17, color='white')
-    plt.legend()
+    # Super title
+    from highlight_text import htext
+    title = f'<{col1}> & <{col2}>'
+    undertitle = f'{team} | {league}'
+    htext.fig_text(0.5, 0.95, s=title,
+                   highlight_textprops=[{'color': c1, 'weight': 'bold'},
+                                        {'color': c2, 'weight': 'bold'}], 
+                   va='center', ha='center',
+                   fontsize=20, color="white")    
+    fig.text(0.5, 0.9, undertitle,
+             color="white", ha='center', va='center', fontsize=12)
+
     ax.spines['bottom'].set_color('white')
     ax.spines['top'].set_color('white')
     ax.spines['left'].set_color('white')
@@ -100,16 +111,29 @@ def create_mavg_chart(df, x, col1, col2, title, y_title, b4a_line, b4a_text, sav
     plt.tick_params(colors='white')
     xmax = int(len(df)*1.1)
     plt.xticks(ticks=range(window, xmax, int(window/2)))
+    y_min_value = np.array([*[df[f'{col1}_mavg'].dropna()], *[df[f'{col2}_mavg'].dropna()]]).min()
     y_max_value = np.array([*[df[f'{col1}_mavg'].dropna()], *[df[f'{col2}_mavg'].dropna()]]).max()
-    plt.yticks(ticks=np.arange(0.4, y_max_value, 0.2))
-    plt.text(xmax-4, y_max_value*1.07,
+    
+    fig.text( 0.86,  0.92,
              'ADAM JAKUS', color=mycolor,
-             fontsize = 15, fontproperties=my_font_props,
+             fontsize = 18, fontproperties=my_font_props,
              ha='center', va='center')
+   
     # Create before-after line
     plt.axvline(x=b4a_line, color=b4a_color, linestyle='--', linewidth=1.5)
     plt.text(b4a_line*0.98, y_max_value*1.05,
              b4a_text, color=b4a_color, va='top', rotation=90)
+    
+    # add logo
+    from PIL import Image
+    import requests
+    from io import BytesIO
+    ax2 = fig.add_axes([0.05, 0.90, 0.1, 0.1])
+    ax2.axis('off')
+    response = requests.get(url_logo)
+    img = Image.open(BytesIO(response.content))
+    ax2.imshow(img)
+    
     plt.tight_layout()
     if save:
         # Save plot
@@ -119,8 +143,22 @@ def create_mavg_chart(df, x, col1, col2, title, y_title, b4a_line, b4a_text, sav
         plt.savefig(save_path, bbox_inches="tight", dpi=300)
     else:
         plt.show()
-        
-create_mavg_chart(df, x='nr', col1='Goals', col2='xG', window=4,
-                  title='Aston Villa Goals & xG', y_title='Value',
-                  b4a_line=33, b4a_text='Debut of winter signings ', b4a_color='orange',
-                  save=True, save_path=r'C:\Users\Adam\Dropbox\TSDP_output\fbref\2025.04', save_name='Aston Villa Goals & xG.png')
+#%% 
+url_logo = 'https://logos-world.net/wp-content/uploads/2020/07/Aston-Villa-Logo.png'
+b4a_line, b4a_text, b4a_color = [33, 'Debut of winter signings','orange']
+y_title = 'Value'
+
+col1, col2 = 'Goals', 'xG'
+create_mavg_chart(df, x='nr', col1=col1, col2=col2, window=4, 
+                  y_title=y_title,
+                  b4a_line=b4a_line, b4a_text= b4a_text, b4a_color=b4a_color,
+                  url_logo=url_logo,
+                  save=False, save_path=r'C:\Users\Adam\Dropbox\TSDP_output\fbref\2025.04', save_name='Aston Villa Goals & xG.png')
+
+#%% 
+col1, col2 = 'Goal/Shot', 'Goal/Shot on Goal'
+create_mavg_chart(df, x='nr', col1=col1, col2=col2, window=4,
+                  y_title=y_title,
+                  b4a_line=b4a_line, b4a_text= b4a_text, b4a_color=b4a_color,
+                  url_logo=url_logo,
+                  save=False, save_path=r'C:\Users\Adam\Dropbox\TSDP_output\fbref\2025.04', save_name='Aston Villa Goals & xG.png')
