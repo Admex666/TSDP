65b6d8c97ac9eb2dee6c039351203ec32133455d
admex666 <adam.jakus99@gmail.com>
2025-05-01 11:39:08 +0200

    roles added to normal_comp

--- fbref/fbref_normal_compare.py
+++ fbref/fbref_normal_compare.py
@@ -1,12 +1,20 @@
 import pandas as pd
 from fbref import fbref_module as fbref
+import numpy as np
 
 df = fbref.get_all_player_data('ESP', year=False)
 
-#%% Determine weights
-pos = 'MF'
+#%% Determine default weights
+pos = 'GK'
+role = 'Sweeper_Keeper'
 matches_min = 5
-tactic = 'low_block'
+tactic = 'default'
+
+df['GA/SoTA%'] = np.where(
+    (df['Performance_SoTA'].notna()) & (df['Performance_SoTA'] != 0),
+    df['Performance_GA'] / df['Performance_SoTA'],
+    np.nan
+)
 
 # Default dictionary
 default = {
@@ -26,7 +34,11 @@ default = {
         'Tackles_Att 3rd': 1.2,
         'Challenges_Tkl': 0.8,
         'Performance_Off': -0.5,
-        'Performance_Fls': -0.4
+        'Performance_Fls': -0.4,
+        'Progression_PrgR': 1.0,
+        'Carries_Mis': -0.8,
+        'Receiving_Rec': 1.0,
+        'Carries_Dis': -0.6
     },
     'MF': {
         'Standard_Sh': 0.7,
@@ -36,8 +48,11 @@ default = {
         'KP': 1.2,
         'Ast': 1.1,
         'Expected_xAG': 1.0,
+        'SCA_SCA90': 1.0,
+        '1/3': 1.0,
         'Carries_PrgDist': 1.2,
         'Carries_1/3': 1.1,
+        'Carries_TotDist': 1.2,
         'Tackles_Tkl': 1.3,
         'Int': 1.3,
         'Performance_Recov': 1.1,
@@ -47,6 +62,7 @@ default = {
     },
     'DF': {
         'Tackles_Tkl': 1.5,
+        'Challenges_Tkl%': 1.3,
         'Int': 1.4,
         'Blocks_Blocks': 1.2,
         'Blocks_Pass': 1.1,
@@ -64,15 +80,96 @@ default = {
     },
     'GK': {
         'Performance_Save%': 1.5,
+        'GA/SoTA%': 1.4,
         'Expected_PSxG+/-': 1.4,
         'Penalty Kicks_Save%': 1.3,
         'Passes_Launch%': 0.5,
         'Total_Cmp': 0.8,
         'Total_Cmp%': 0.8,
-        'Performance_CrdR': -0.8
+        'Performance_CrdR': -0.8,
+        'Performance_CS%': 1.2,
+        'Sweeper_#OPA/90': 0.8,
+        'Crosses_Stp%': 1.1
     }
 }
 
+
+#%% Positional weights
+positional_weights = {
+    'Ball_Playing_Defender': default['DF'].copy(),
+    'Deep_Lying_Playmaker': default['MF'].copy(),
+    'Ball_Winning_Midfielder': default['MF'].copy(),
+    'Inside_Forward': default['FW'].copy(),
+    'Poacher': default['FW'].copy(),
+    'Sweeper_Keeper': default['GK'].copy()
+}
+
+# Ball Playing Defender – magasabb passz-, technika- és vonalbontó képesség:
+positional_weights['Ball_Playing_Defender'].update({
+    'Total_Cmp': 1.3,
+    'Total_Cmp%': 1.1,
+    'Long_Cmp%': 1.2,
+    'Carries_PrgDist': 1.3,
+    'Carries_Carries': 1.2,
+    'Sweeper_#OPA/90': 1.1,
+    'Err': -1.2
+})
+
+# Deep Lying Playmaker – passzátlagok, kreativitás és labdabiztonság:
+positional_weights['Deep_Lying_Playmaker'].update({
+    'Total_Cmp%': 1.5,
+    'PrgP': 1.4,
+    'KP': 1.3,
+    'SCA_SCA90': 1.2,
+    'Touches_Touches': 1.2,
+    'Carries_TotDist': 1.1,
+    'Performance_CrdY': -0.6
+})
+
+# Ball Winning Midfielder – szerelések, labdaszerzés és fizikai aktivitás:
+positional_weights['Ball_Winning_Midfielder'].update({
+    'Tackles_Tkl': 1.6,
+    'Challenges_Tkl%': 1.5,
+    'Tkl+Int': 1.4,
+    'Performance_Int': 1.4,
+    'Touches_Live': 1.2,
+    'Performance_Fld': 1.0
+})
+
+# Inside Forward – befejezés, 1-az-1, kreatív passzok:
+positional_weights['Inside_Forward'].update({
+    'Performance_Gls': 1.7,
+    'Expected_npxG': 1.6,
+    'Progression_PrgR': 1.1,
+    'Take-Ons_Succ': 1.2,
+    'Performance_SoT': 1.3,
+    'Ast': 1.1,
+    'Receiving_Rec': 1.0
+})
+
+# Poacher – kizárólag befejezés és helyzetkihasználás:
+positional_weights['Poacher'].update({
+    'Performance_Gls': 2.0,
+    'Expected_xG': 1.8,
+    'Expected_npxG': 1.8,
+    'Performance_SoT': 1.5,
+    'Standard_Sh': 1.2,
+    'Carries_PrgC': 0.6,
+    'Tackles_Att 3rd': 0.3
+})
+
+# Sweeper Keeper – mentések, sweeper-akciók és vonalbontó passzok:
+positional_weights['Sweeper_Keeper'].update({
+    'Performance_Save%': 1.6,
+    'Expected_PSxG+/-': 1.5,
+    'Sweeper_#OPA/90': 1.3,
+    'Crosses_Stp%': 1.2,
+    'GA/SoTA%': 1.4,
+    'Passes_Launch%': 1.1,
+    'Total_Cmp%': 0.9
+})
+
+#%% Tactical weights
 tweights = {
     'default': default,
 
@@ -269,15 +366,22 @@ tweights = {
     }
 }
 
-
+#%% Filter df and Calculate avg, std, weights
+position_or_tactic = 'p'
+if position_or_tactic == 'p':
+    weight_dict = positional_weights[role]
+else:
+    weight_dict = tweights[tactic][pos]
+    
 mask = (df.Pos.str.contains(pos)) & (df['90s']>matches_min)
 df_filter = df[mask].reset_index(drop=True)
 
 cols_basic = ['Rk', 'Player', 'Nation', 'Pos', 'Squad', 'Age', 'Born', '90s']
 # Select statistics for comparison
-stats_list = list(tweights[tactic][pos].keys())
+stats_list = list(weight_dict.keys())
 
 df_final = df_filter[cols_basic+stats_list]
+df_final.fillna(0, inplace=True)
 
 # per90 values
 df_final90 = df_final.copy()
@@ -297,7 +401,7 @@ df_avgs.loc['std', :] = df_final90[stats_list90].std()
 ## Set importances manually
 #weights = []
 #df_avgs.loc['weight', :] = float(1)
-df_avgs.loc['weight', :] = list(tweights[tactic][pos].values())
+df_avgs.loc['weight', :] = list(weight_dict.values())
 weights_sum = df_avgs.loc['weight', :].sum()
 
 #%% Normalize, calculate index
@@ -322,7 +426,7 @@ for i, col, col90 in zip(range(len(stats_list)), stats_list, stats_list90):
         else:
             newcol = stats_dict[col]['name']
         df_final90_normal.rename(columns={col90: newcol}, inplace=True)
-df_final90_normal['index'] = (df_final90_normal['score'] / (weights_sum*1.5)*100).astype(int)
+df_final90_normal['index'] = round((df_final90_normal['score'] / (weights_sum*1.5)*100), 1)
 
 print(df_final90_normal[['Player', 'Squad', 'score', 'index']].sort_values(by='score', ascending=False).head(10))
 
