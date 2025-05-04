25334e213cd149b2b6c07839c1f718f05c808740
admex666 <adam.jakus99@gmail.com>
2025-05-04 15:06:21 +0200

    added all roles

--- fbref/fbref_normal_compare.py
+++ fbref/fbref_normal_compare.py
@@ -5,11 +5,12 @@ import numpy as np
 df = fbref.get_all_player_data('ESP', year=False)
 
 #%% Determine default weights
-pos = 'GK'
-role = 'Sweeper_Keeper'
+pos = 'DF'
+role = 'Full-Back'
 matches_min = 5
 tactic = 'default'
 
+#%% Default dictionary
 df['GA/SoTA%'] = np.where(
     (df['Performance_SoTA'].notna()) & (df['Performance_SoTA'] != 0),
     df['Performance_GA'] / df['Performance_SoTA'],
@@ -22,7 +23,6 @@ default = {
         'Performance_Gls': 1.8,
         'Expected_npxG': 1.5,
         'Standard_SoT': 1.4,
-        'Standard_Sh': 1,
         'KP': 1.2,
         'Ast': 1.1,
         'Expected_xAG': 1.3,
@@ -35,7 +35,6 @@ default = {
         'Challenges_Tkl': 0.8,
         'Performance_Off': -0.5,
         'Performance_Fls': -0.4,
-        'Progression_PrgR': 1.0,
         'Carries_Mis': -0.8,
         'Receiving_Rec': 1.0,
         'Carries_Dis': -0.6
@@ -95,17 +94,23 @@ default = {
 
 
 #%% Positional weights
-positional_weights = {
-    'Ball_Playing_Defender': default['DF'].copy(),
-    'Deep_Lying_Playmaker': default['MF'].copy(),
-    'Ball_Winning_Midfielder': default['MF'].copy(),
-    'Inside_Forward': default['FW'].copy(),
-    'Poacher': default['FW'].copy(),
-    'Sweeper_Keeper': default['GK'].copy()
+position_roles = {
+    'DF': ['Central Defender', 'Ball Playing Defender', 'Libero', 
+           'Full-Back', 'Wing-Back', 'Inverted Wing-Back'],
+    'MF': ['Defensive Midfielder', 'Ball Winning Midfielder', 'Deep Lying Playmaker',
+           'Box to Box Midfielder', 'Mezzala', 'Central Midfielder', 
+           'Advanced Playmaker', 'Shadow Striker', 'Attacking Midfielder'],
+    'FW': ['Advanced Forward', 'Complete Forward', 'Poacher', 'Inside Forward', 'Target Man'],
+    'GK': ['Goalkeeper', 'Sweeper Keeper']
 }
 
+positional_weights = {}
+for p in position_roles.keys():
+    for r in position_roles[p]:
+        positional_weights[r] = default[p].copy()
+
 # Ball Playing Defender – magasabb passz-, technika- és vonalbontó képesség:
-positional_weights['Ball_Playing_Defender'].update({
+positional_weights['Ball Playing Defender'].update({
     'Total_Cmp': 1.3,
     'Total_Cmp%': 1.1,
     'Long_Cmp%': 1.2,
@@ -116,7 +121,7 @@ positional_weights['Ball_Playing_Defender'].update({
 })
 
 # Deep Lying Playmaker – passzátlagok, kreativitás és labdabiztonság:
-positional_weights['Deep_Lying_Playmaker'].update({
+positional_weights['Deep Lying Playmaker'].update({
     'Total_Cmp%': 1.5,
     'PrgP': 1.4,
     'KP': 1.3,
@@ -127,22 +132,20 @@ positional_weights['Deep_Lying_Playmaker'].update({
 })
 
 # Ball Winning Midfielder – szerelések, labdaszerzés és fizikai aktivitás:
-positional_weights['Ball_Winning_Midfielder'].update({
+positional_weights['Ball Winning Midfielder'].update({
     'Tackles_Tkl': 1.6,
     'Challenges_Tkl%': 1.5,
-    'Tkl+Int': 1.4,
     'Performance_Int': 1.4,
     'Touches_Live': 1.2,
     'Performance_Fld': 1.0
 })
 
 # Inside Forward – befejezés, 1-az-1, kreatív passzok:
-positional_weights['Inside_Forward'].update({
+positional_weights['Inside Forward'].update({
     'Performance_Gls': 1.7,
     'Expected_npxG': 1.6,
-    'Progression_PrgR': 1.1,
     'Take-Ons_Succ': 1.2,
-    'Performance_SoT': 1.3,
+    'Standard_SoT': 1.3,
     'Ast': 1.1,
     'Receiving_Rec': 1.0
 })
@@ -152,14 +155,13 @@ positional_weights['Poacher'].update({
     'Performance_Gls': 2.0,
     'Expected_xG': 1.8,
     'Expected_npxG': 1.8,
-    'Performance_SoT': 1.5,
-    'Standard_Sh': 1.2,
+    'Standard_SoT': 1.5,
     'Carries_PrgC': 0.6,
     'Tackles_Att 3rd': 0.3
 })
 
 # Sweeper Keeper – mentések, sweeper-akciók és vonalbontó passzok:
-positional_weights['Sweeper_Keeper'].update({
+positional_weights['Sweeper Keeper'].update({
     'Performance_Save%': 1.6,
     'Expected_PSxG+/-': 1.5,
     'Sweeper_#OPA/90': 1.3,
@@ -168,6 +170,159 @@ positional_weights['Sweeper_Keeper'].update({
     'Passes_Launch%': 1.1,
     'Total_Cmp%': 0.9
 })
+
+# Central Defender – hagyományos középső védő, a szerelés és blokkolás dominál:
+positional_weights['Central Defender'].update({
+    'Tackles_Tkl': 1.6,
+    'Blocks_Blocks': 1.4,
+    'Blocks_Pass': 1.3,
+    'Clr': 1.4,
+    'Aerial Duels_Won%': 1.5,
+    'Performance_OG': -0.9,
+    'Err': -1.2
+})
+
+# Libero – vonalbontó, technikai védő aki beljebb jön:
+positional_weights['Libero'].update({
+    'Total_Cmp': 1.3,
+    'PrgP': 1.2,
+    'Carries_PrgDist': 1.2,
+    'Sweeper_#OPA/90': 1.2,
+    'Touches_Def 3rd': 1.1,
+    'Err': -1.0
+})
+
+# Full-Back – klasszikus szélső védő, támogató passzok és blokkok:
+positional_weights['Full-Back'].update({
+    'Performance_Crs': 1.2,
+    'CrsPA': 1.1,
+    'Total_Cmp%': 1.1,
+    'Short_Cmp%': 1.1,
+    'Tackles_Tkl': 1.3,
+    'Performance_Int': 1.2
+})
+
+# Wing-Back – támadóbb full-back, sok beívelés és előrevitele:
+positional_weights['Wing-Back'].update({
+    'Performance_Crs': 1.3,
+    'CrsPA': 1.2,
+    'Touches_Att 3rd': 1.2,
+    'Carries_PrgC': 1.1,
+    'Tackles_Tkl': 1.2,
+    'Performance_Recov': 1.1
+})
+
+# Inverted Wing-Back – beljebb húzódó védő, passzjáték és progresszió:
+positional_weights['Inverted Wing-Back'].update({
+    'Total_Cmp%': 1.2,
+    'PrgP': 1.1,
+    'Carries_PrgDist': 1.1,
+    'Tackles_Def 3rd': 1.2,
+    'Aerial Duels_Won%': 1.1
+})
+
+# Defensive Midfielder – elsődleges védekezés, szerelések, labdaszerzés:
+positional_weights['Defensive Midfielder'].update({
+    'Tackles_Tkl': 1.6,
+    'Challenges_Tkl%': 1.4,
+    'Performance_Int': 1.4,
+    'Performance_Recov': 1.3,
+    'Touches_Def 3rd': 1.1
+})
+
+# Box to Box Midfielder – sokoldalú, támadás és védekezés is fontos:
+positional_weights['Box to Box Midfielder'].update({
+    'Tackles_Tkl': 1.4,
+    'Performance_Recov': 1.2,
+    'Carries_PrgDist': 1.2,
+    'PrgP': 1.2,
+    'Ast': 1.1,
+    'Performance_Gls': 1.1
+})
+
+# Mezzala – belső támadó középpályás, kreatív és progresszív passzok:
+positional_weights['Mezzala'].update({
+    'PrgP': 1.4,
+    'KP': 1.3,
+    'Carries_PrgDist': 1.2,
+    'SCA_SCA90': 1.2,
+    'Touches_Mid 3rd': 1.1
+})
+
+# Central Midfielder – kiegyensúlyozott box-to-box/support szerep:
+positional_weights['Central Midfielder'].update({
+    'Total_Cmp%': 1.3,
+    'Carries_TotDist': 1.2,
+    'PrgP': 1.2,
+    'Ast': 1.0,
+    'Performance_Fld': 1.0
+})
+
+# Advanced Playmaker – kreatív szervező, passzpontosság és helyzetkeltés:
+positional_weights['Advanced Playmaker'].update({
+    'Total_Cmp%': 1.5,
+    'PrgP': 1.4,
+    'KP': 1.4,
+    'Expected_xAG': 1.3,
+    'SCA_SCA90': 1.3,
+    'Performance_CrdY': -0.5
+})
+
+# Shadow Striker – mögöttes csatár, gólérzékeny és helyzetkihasználó:
+positional_weights['Shadow Striker'].update({
+    'Performance_Gls': 1.7,
+    'Expected_npxG': 1.6,
+    'Standard_SoT': 1.4,
+    'SCA_SCA90': 1.2,
+    'Ast': 1.1,
+    'Tackles_Att 3rd': 1.0
+})
+
+# Attacking Midfielder – középcsatár-szervező, passzok és lövések:
+positional_weights['Attacking Midfielder'].update({
+    'KP': 1.3,
+    'Expected_xAG': 1.2,
+    'Performance_Ast': 1.2,
+    'Performance_Gls': 1.2,
+    'PrgP': 1.1
+})
+
+# Advanced Forward – gyors, direkt, mélységi indításokra optimalizált:
+positional_weights['Advanced Forward'].update({
+    'Performance_Gls': 1.8,
+    'Expected_npxG': 1.6,
+    'Standard_SoT': 1.5,
+    'Take-Ons_Att': 1.1,
+    'Take-Ons_Succ': 1.2,
+    'Carries_PrgC': 1.1,
+    'Tackles_Att 3rd': 0.8
+})
+
+# Complete Forward – minden elemben (térkihasználás, tartás, kreativitás) jól szerepel:
+positional_weights['Complete Forward'].update({
+    'Performance_Gls': 1.6,
+    'Expected_xG': 1.5,
+    'Expected_xAG': 1.4,
+    'KP': 1.3,
+    'Ast': 1.2,
+    'Carries_PrgC': 1.2,
+    'Carries_1/3': 1.1,
+    'Tackles_Att 3rd': 1.0,
+    'Challenges_Tkl': 1.0
+})
+
+# Target Man – erős testfelépítés, letámadás, holt labdás játékok:
+positional_weights['Target Man'].update({
+    'Aerial Duels_Won%': 2.0,
+    'Aerial Duels_Won': 1.7,
+    'Performance_Gls': 1.2,
+    'Standard_SoT': 1.1,
+    'Carries_Carries': 1.3,
+    'Carries_TotDist': 1.2,
+    'Performance_Crs': 1.0,
+    'Performance_Fls': -0.2
+})
+
 
 #%% Tactical weights
 tweights = {
@@ -367,11 +522,12 @@ tweights = {
 }
 
 #%% Filter df and Calculate avg, std, weights
-position_or_tactic = 'p'
-if position_or_tactic == 'p':
-    weight_dict = positional_weights[role]
+if role == '':
+    weight_dict = tweights[tactic][pos].copy()
+elif role == 'default':
+    weight_dict = default[pos].copy()
 else:
-    weight_dict = tweights[tactic][pos]
+    weight_dict = positional_weights[role].copy()
     
 mask = (df.Pos.str.contains(pos)) & (df['90s']>matches_min)
 df_filter = df[mask].reset_index(drop=True)
@@ -428,7 +584,7 @@ for i, col, col90 in zip(range(len(stats_list)), stats_list, stats_list90):
         df_final90_normal.rename(columns={col90: newcol}, inplace=True)
 df_final90_normal['index'] = round((df_final90_normal['score'] / (weights_sum*1.5)*100), 1)
 
-print(df_final90_normal[['Player', 'Squad', 'score', 'index']].sort_values(by='score', ascending=False).head(10))
+print(df_final90_normal[['Pos', 'Player', 'Squad', 'score', 'index']].sort_values(by='score', ascending=False).head(10))
 
 #%% Compare each tactic to default
 for t in list(tweights.keys())[1:]:
@@ -439,3 +595,14 @@ for t in list(tweights.keys())[1:]:
             w_tactic = tweights[t][p][s]
             diff = int( (w_tactic / w_def -1) *100 )
             print(f'{s} diff.: {diff}%')
+            
+            
+#%% Correlations
+df_corr_input = df_final[stats_list]
+correlation_matrix = df_corr_input.corr(method='pearson')
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+plt.show()
